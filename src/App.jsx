f4d2f8d3cfcf312f695b6463950ae94f37a40a39
Abinanthan47 <abinanthan247@gmail.com>
2025-02-10 17:27:51 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="h-screen relative w-full">
+      <img src='./back-sky.webp' className='h-full w-full object-cover' />
+      <h1 className='text-4xl md:text-6xl  font-medium lg:text-8xl text-white absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center px-4'>
+        ⚠️  SITE UNDER CONSTRUCTION
+      </h1>
+    </div>
+  )
+}
+
+export default App
