44ca356c0883853718ed5a4c7fbb1e8d80c5e641
Abinanthan <143276733+Abinanthan47@users.noreply.github.com>
2025-02-18 12:48:54 +0530

    Update App.jsx analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/react"
 import CountdownTimer from "./components/CountdownTimer"
 import Footer from "./components/Footer"
 import Gallery from "./components/Gallery"
@@ -8,19 +9,18 @@ import TechDay1 from "./components/TechEventsDay1"
 import TechDay2 from "./components/TechEventsDay2"
 import Workshops from "./components/WorkShop"
 
-
-
 function App() {
   return <>
-  <CountdownTimer/>
+    <Analytics />
+    <CountdownTimer />
     <Hero />
     <TechDay1 />
     <TechDay2 />
-    <ImageCarousel/>
-    <FuzzyOverlayExample/>
-   <Workshops/>
-   <Gallery/>
-   <Footer/>
+    <ImageCarousel />
+    <FuzzyOverlayExample />
+    <Workshops />
+    <Gallery />
+    <Footer />
   </>
 }
 
